3916866a9ca992063d06b5eee093eb9c22c8dd98
Михаил Горбань <91434795+usernameisavailablee@users.noreply.github.com>
2024-10-04 17:29:13 +0300

    second start

---
+++ BookTrading/src/BookSellerAgent.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author michael
+ */
+import jade.core.Agent;
+
+public class BookSellerAgent extends Agent  {
+    protected void setup(){
+        System.out.println("Seller-agent "+ getAID().getName()+" is ready");
+    }
+    
+}
